d8b72c894bf5b57c7d525d75be1f80434987242b
1fexd <PGFvy85nC@protonmail.com>
2023-06-11 16:02:17 +0200

    feat!: Store mime type map in actual map instead of loading from json

--- src/main/kotlin/MimeTypeResource.kt
+++
@@ -1,6 +0,0 @@
-import java.io.InputStream
-
-object MimeTypeResource {
-    fun getBuiltInMimeTypeJson(name: String = "mimetypes.json"): InputStream? =
-        MimeTypeResource::class.java.getResourceAsStream(name)
-}
--- src/main/resources/parse-mimetypes.py
+++ src/main/resources/parse-mimetypes.py
@@ -1,4 +1,4 @@
-import json
+from textwrap import dedent
 import xml.etree.ElementTree as ElementTree
 
 tree = ElementTree.parse("tika-mimetypes.xml")
@@ -11,5 +11,45 @@ for mime_type in root:
     if len(globs) > 0:
         result.append({"type": _type, "extensions": globs})
 
-with open("mimetypes.json", "w") as file:
-    json.dump(result, file)
+
+def write_new_line(_file, _str, indent="", clean=False):
+    write(_file, _str, indent, clean)
+    _file.write("\n")
+
+
+def write(_file, _str, indent="", clean=False):
+    if clean:
+        cleaned_str = dedent(_str)
+        indented_str = ""
+        lines = cleaned_str.splitlines()
+        for index, line in enumerate(lines):
+            indented_str += indent + line
+            if index < len(lines) - 1:
+                indented_str += "\n"
+
+        _str = indented_str
+
+    _file.write(_str)
+
+
+with open("../kotlin/fe/mimetypekt/MimeTypes.kt", "w") as kt_file:
+    write_new_line(kt_file, """
+        package fe.mimetypekt
+        object MimeTypes {
+            val mimeTypeToExtensions = mapOf(
+    """, clean=True)
+
+    for mime_type in result:
+        extension_str = ", ".join([f"\"{extension}\"" for extension in mime_type['extensions']])
+        write_new_line(kt_file, f"\t\t\"{mime_type['type']}\" to listOf({extension_str}),")
+
+    write_new_line(kt_file, "\t)")
+    write_new_line(kt_file, """
+        val extensionToMimeType = mutableMapOf<String, String>().apply {
+            mimeTypeToExtensions.forEach { (mimeType, extensions) ->
+                extensions.forEach { put(it, mimeType) }
+            }
+        }.toMap()
+    """, indent="\t", clean=True)
+
+    kt_file.write("}")
